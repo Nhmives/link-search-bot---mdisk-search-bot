45477063f3952943cfe939673c74184dcdb08640
Nhmives <nhmovies9@gmail.com>
2025-02-28 19:55:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,7 +7,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "Teraboxlink_search_bot")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", 1-1002325240013))
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -1002325240013))
     BOT_USERNAME = os.environ.get("BOT_USERNAME")
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
     DATABASE_URL = os.environ.get("DATABASE_URL")
