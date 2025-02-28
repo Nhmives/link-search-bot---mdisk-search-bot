c365dfe13730c9fc3cf1419c2b789f7196c71456
Nhmives <nhmovies9@gmail.com>
2025-03-01 00:24:25 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,19 +2,21 @@ import os
 
 
 class Config(object):
-    API_ID = os.environ.get("API_ID", "")
+    API_ID = int(os.environ.get("API_ID", ))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "LinkSearchBot")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Teraboxlink_search_bot")
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME")
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
     DATABASE_URL = os.environ.get("DATABASE_URL")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     ABOUT_BOT_TEXT = """<b>This is Link Search Bot.
     
     
-🤖 My Name: <a href='https://t.me/greymatters_about'>Link Search Bot</a>
+    
+🤖 My Name: <a href='https://youtube.com/@GreyMattersBot'>Link Search Bot</a>
 
 📝 Language : <a href='https://www.python.org'> Python V3</a>
 
@@ -22,10 +24,10 @@ class Config(object):
 
 📡 Server: <a href='koyeb.com'>Koyeb</a>
 
-👨‍💻 Created By: <a href='https://t.me/greymatters_about'>mr Nazbul's Bot</a></b>
+👨‍💻 Created By: <a href='https://t.me/GreyMatter_Bot'>GreyMatter's Bot</a></b>
 """
 
-    ABOUT_HELP_TEXT = """<b>👨‍💻 Creator : <a href='https://t.me/greymatters_about'>Mr Nazbul's Bot</a>
+    ABOUT_HELP_TEXT = """<b>👨‍💻 Creator : <a href='https://t.me/GreyMatter_Bot'>GreyMatter's Bot</a>
 If You Want Your Own Bot Like This Then You Can Contact Our Creator.</b>
 """
 
@@ -36,7 +38,7 @@ I'm Link Search Bot.🤖
 
 I Can Search 🔍 What You Want❗
 
-<a>Made With ❤ By @greymatters_about</a></b>
+<a>Made With ❤ By @GreyMatter_Bots</a></b>
 """
 
 
@@ -47,6 +49,6 @@ I'm Link Search Bot.🤖
 
 I Can Search 🔍 What You Want❗
 
-<a>Made With ❤ By @greymatters_about</a></b>
+<a>Made With ❤ By @GreyMatter_Bots</a></b>
 """
-
+    
